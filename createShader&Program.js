1cb025247a38e0b7af90a05e90b2658c2c6722f2
Toxicfy <15927630261@163.com>
2022-03-17 23:43:09 +0800

    feat: createShader & Program

---
+++ createShader&Program.js
@@ -0,0 +1,51 @@
+/**
+ * 创建着色器
+ * @param {*} gl 渲染上下文
+ * @param {*} type 着色器类型
+ * @param {*} source 数据源 glsl 
+ * @returns shader
+ */
+const createShader = (gl, type, source) => {
+    const shader = gl.createShader(type) // 着色器对象生成
+    gl.shaderSource(shader, source) // 数据源关联
+
+    gl.completeShader(shader) // 编译生成着色器
+
+    const status = gl.getShaderParameter(shader, gl.COMPLICE_STATUS)
+
+    // 如果创建成功，则返回 shader
+    if (status) {
+        return shader
+    }
+
+    gl.deleteShader(shader)
+}
+
+/**
+ * 生成着色器程序
+ * @param {*} gl 渲染上下文
+ * @param {*} vertexShader 顶点着色器
+ * @param {*} fragmentShader 片段着色器
+ * @returns 着色器程序
+ */
+const createProgram = (gl, vertexShader, fragmentShader) => {
+   
+    const program = gl.createProgram()  // 创建着色器程序并链接 shader
+    gl.attachShader(program, vertexShader)
+    gl.attachShader(program, fragmentShader)
+
+    gl.linkProgram(program)
+
+    const status = gl.getProgramParameter(program, gl.LINK_STATUS)
+
+    if (status) {
+        return program
+    }
+
+    gl.deleteProgram(program)
+}
+
+export default {
+    createProgram,
+    createShader
+}
--- drawPoint.js
+++ drawPoint.js
@@ -1,5 +1,5 @@
 /**
- * 想法：glsl 中内置了一些变量，直接可以使用，不需要申明
+ *  glsl 中内置了一些变量，直接可以使用，不需要申明
  */
 const canvas = document.createElement('canvas')
 
@@ -9,22 +9,23 @@ const gl = canvas.getContext('webgl') // 获取 webgl 上下文
 // gl_Position 是一个顶点着色器主要设置的变量
 const vertexShaderSource = `
     void main() {
-        gl_PointSize = 20.0; // 像素大小
-        gl_position = vec4(0.0, 0.0, 0.0, 1.0); // 指定位置在原点
+        gl_PointSize = 20.0;
+        gl_Position = vec4(0.0,0.0,0.0,1.0);
     }
 `
 // gl_FragColor是一个片元着色器主要设置的变量
 const fragmentShaderSource = `
     void main() {
-        //  颜色范围 [0,1], 类似于 rgba
         gl_FragColor = vec4(1, 0, 0.5, 1);
     }
 `
 
 const initShader = (gl, vertexShaderSource, fragmentShaderSource) => {
+  // 使用 gl.createShader() 创建新的着色器
   let vertexShader = gl.createShader(gl.VERTEX_SHADER)
   let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
 
+  // gl.shaderSource() 将源代码发送到着色器
   gl.shaderSource(vertexShader, vertexShaderSource)
   gl.shaderSource(fragmentShader, fragmentShaderSource)
 
@@ -44,3 +45,5 @@ const initShader = (gl, vertexShaderSource, fragmentShaderSource) => {
 
 const program = initShader(gl, vertexShaderSource, fragmentShaderSource)
 gl.drawArrays(gl.POINTS, 0, 1)
+
+document.body.appendChild(canvas)
